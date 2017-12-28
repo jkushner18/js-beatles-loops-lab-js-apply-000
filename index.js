64c6e39719055bdb954981630f4c9665f4db18a6
Jason Kushner <jkushner18@gmail.com>
2017-12-28 02:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(array1, array2){
   
   for(var i = 0; i<=3;i++){
     
-  final =+ array1[i] + " plays " + array2[i] + "."}
+  final = array1[i] + " plays " + array2[i] + "."}
   
   return final
      

fd901138b410f80c10e29211b2d6a4d67dde2713
Jason Kushner <jkushner18@gmail.com>
2017-12-28 02:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
- 
+ const facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice" ]
 
 function theBeatlesPlay(array1, array2){
   

b0e9be1172f671381fa596d0a4eef21e8fc81820
Jason Kushner <jkushner18@gmail.com>
2017-12-28 02:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-
+function theBeatlesPlay(array1, array2){
+  
+  var final=[];
+  
+  for(var i = 0; i<=3;i++){
+    
+  final.push(array1[i] + " plays " + array2[i] + ".")
+     
+} return final }
 

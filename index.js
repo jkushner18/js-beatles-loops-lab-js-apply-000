d2999efa86366045111e4dffa3403af89b76146a
Jason Kushner <jkushner18@gmail.com>
2017-12-28 02:08:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+arrmusician=["John Lennon"."George Harrison","Ringo Starr","Paul Mccartney"]
+
+arr
+
+
+
+function theBealtesPlay(arrmusician, arrinstr){
+  
+  var final=[];
+  
+  for(var i = 0; i<arrmusician.length-1;i++){
+    
+  
+    
+    
+  }
+  
+  
+}

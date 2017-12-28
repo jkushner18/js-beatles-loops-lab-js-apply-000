7b5336751803f7a01e970cab0539756135d9c7f8
Jason Kushner <jkushner18@gmail.com>
2017-12-28 02:14:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-arrmusician=["John Lennon"."George Harrison","Ringo Starr","Paul Mccartney"];
+arrmusician=["John Lennon","George Harrison","Ringo Starr","Paul Mccartney"];
 
 arrinstr=["guitar","drums","bass","Keyboard"];
 
